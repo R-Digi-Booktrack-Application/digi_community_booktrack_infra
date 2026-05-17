bcdeee4c4d836d4afb670f65803cd6833eb2083f
revathyshunmugham <srevathy22@gmail.com>
2026-05-17 10:14:14 +0530

    initial deployment

--- pillars/dev/backend.tf
+++ pillars/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "digi-dev-tf-s3-bucket"
+    bucket         = "digi-dev-tf-s3-bucket-revathy"
     key            = "eks/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "digi-dev-tf-lock-state"
