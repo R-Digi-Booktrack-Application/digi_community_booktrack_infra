6ca2db209c522a494c374def634e7a57c23fa551
revathyshunmugham <srevathy22@gmail.com>
2026-06-08 11:02:16 +0530

    Initial commit

---
+++ modules/eks/variables.tf
@@ -0,0 +1,30 @@
+variable "cluster_name" { type = string }
+variable "kubernetes_version" { type = string }
+variable "node_group_name" { type = string }
+
+variable "desired_size" { type = number }
+variable "max_size" { type = number }
+variable "min_size" { type = number }
+
+variable "pillar_name" { type = string }
+variable "customer_name" { type = string }
+variable "file_name" { type = string }
+
+variable "subnet_ids" {
+  type    = list(string)
+  default = []
+}
+
+variable "security_group_ids" {
+  type    = list(string)
+  default = []
+}
+
+variable "eks_addons" {
+  description = "Map of EKS addons and their versions"
+  type = map(object({
+    addon_version = string
+  }))
+
+  default = {}
+}
---
+++ pillars/acc/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "digi-dev-tf-s3-bucket"
+    key            = "eks/dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "digi-dev-tf-lock-state"
+  }
+}
